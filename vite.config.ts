f4aa29d8eda52c1273d3aa5b8f8596aa20962d75
千葉 智貴 <132980385+kentem-to-chiba@users.noreply.github.com>
2024-05-17 23:02:22 +0900

    GitHub Pagesにデプロイするための設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  base: process.env.GITHUB_PAGES ? "todoapp-with-supabase" : "./",
+  plugins: [react()],
 });
